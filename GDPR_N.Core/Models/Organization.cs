9cb9f28ca983e2e9c070c51a6e3be49fe42218de
Aaron <develop44242@tuta.com>
2025-11-01 14:10:12 -0600

    Add SubscriptionTier property to Organization class

--- GDPR_N.Core/Models/Organization.cs
+++ GDPR_N.Core/Models/Organization.cs
@@ -1,8 +1,11 @@
+using GDPR_N.Core.Models.Enumerations;
+
 namespace GDPR_N.Core.Models;
 
 public class Organization
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Industry { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Industry { get; set; } = string.Empty;
+    public SubscriptionTier SubscriptionTier { get; set; }
 }
